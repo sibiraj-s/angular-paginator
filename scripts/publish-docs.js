727045dc2ad45ab401ec243fb3dc8aa2664389c9
Sibiraj <dev.sibiraj@outlook.com>
2018-10-10 23:07:26 +0530

    docs: use gh-pages to publish docs

---
+++ scripts/publish-docs.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const inquirer = require('inquirer');
+const ghpages = require('gh-pages');
+const chalk = require('chalk');
+const packageJson = require('../package.json');
+
+const ghPagesOptions = {
+  branch: 'gh-pages',
+  message: `docs: ${packageJson.name} v${packageJson.version}`
+};
+
+inquirer.prompt([
+  {
+    name: 'publishDocs',
+    type: 'confirm',
+    message: `Do You want to publish the docs to '${chalk.cyan(ghPagesOptions.branch)}' branch?`
+  }
+]).then((answers) => {
+  if (answers.publishDocs) {
+    ghpages.publish('docs', ghPagesOptions, (err) => {
+      if (err) {
+        chalk.red('Unable to publish docs. Error: ', err);
+      }
+      console.log(chalk.green(`\nDocumentation published successfully to ${chalk.cyan('\'gh-pages\'')} \n`));
+    });
+  }
+});
