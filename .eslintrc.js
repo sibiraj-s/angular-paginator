be8dec58644079608ece0e9b65b16b78316f4989
sibiraj-s <dev.sibiraj@outlook.com>
2020-09-10 15:33:55 +0530

    chore: use pegasus eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,9 @@
 module.exports = {
-  extends: 'airbnb-base',
-  parserOptions: {
-    sourceType: 'script'
-  },
+  extends: [
+    'pegasus',
+    'pegasus/node'
+  ],
   rules: {
-    'no-console': 'off',
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'comma-dangle': ['error', 'never']
+    'no-console': 'off'
   }
 };
